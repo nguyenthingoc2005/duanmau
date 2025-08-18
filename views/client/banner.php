8abcee2c33d1613e65461961fa06c6e180fba534
ngoc <nguyenthingoc270805@gmail.com>
2025-08-18 20:23:39 +0700

    update code

--- views/client/banner.php
+++ views/client/banner.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="vi">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trang slideshow ảnh</title>
+    <title>Trang Slideshow Ảnh</title>
     <style>
         /* CSS cho slideshow */
         .slideshow-container {
             height: 90svh;
             width: 100%;
-
+         
             overflow: hidden;
         }
 
@@ -25,48 +24,52 @@
             height: 100%;
             object-fit: cover;
         }
-
+        
         .fade {
             animation-name: fade;
             animation-duration: 1.5s;
         }
 
         @keyframes fade {
-            from {
-                opacity: .4
-            }
-
-            to {
-                opacity: 1
-            }
+            from {opacity: .4}
+            to {opacity: 1}
         }
     </style>
 </head>
-
 <body>
+
 <?php
-    $images = [
-        'https://theme.hstatic.net/1000344185/1001270599/14/slideshow_1.jpg?v=372',
-        "https://theme.hstatic.net/1000344185/1001270599/14/slideshow_2.jpg?v=372",
-        "https://theme.hstatic.net/1000344185/1001270599/14/imgaView5.jpg?v=372"
-    ];
+$images = [
+  'https://theme.hstatic.net/1000344185/1001270599/14/slideshow_1.jpg?v=372',
+  "https://theme.hstatic.net/1000344185/1001270599/14/slideshow_2.jpg?v=372",
+  "https://theme.hstatic.net/1000344185/1001270599/14/imgaView5.jpg?v=372"
+];
 ?>
+
+<div class="slideshow-container">
+    <?php foreach ($images as $image): ?>
+        <div class="mySlides fade">
+            <img src="<?= htmlspecialchars($image); ?>" alt="Ảnh Slideshow">
+        </div>
+    <?php endforeach; ?>
+</div>
+
 <script>
     let slideIndex = 0;
-    showslides();
-    function showSlides(){
+    showSlides();
+
+    function showSlides() {
         let i;
         let slides = document.getElementsByClassName("mySlides");
-        for(i=0; i< slides.length; i++){
-            slides[i].style.display = "none";
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
         }
         slideIndex++;
-        if(slideIndex > slides.length) {slideIndex = 1}
-        slides[slideIndex-1].style.display = "block";
-        setTimeout(showSlides, 2500);
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        slides[slideIndex-1].style.display = "block";  
+        setTimeout(showSlides, 2000); // Tự động chuyển đổi sau 3 giây
     }
 </script>
 
 </body>
-
 </html>
